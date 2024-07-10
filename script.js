1a494339fbe50731bdbde9023d506f0e3ee99873
Kshitij8576 <shuklakshitij161@gmail.com>
2024-07-10 16:10:33 +0530

    Sixth commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed =new Typed(".type",{
+    strings:["","Web Designer","Web Developer","Photo Editor","Video Editor"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
+var typed =new Typed(".section2 h2 span",{
+    strings:["","Web Designer","Web Developer","Photo Editor","Video Editor"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
